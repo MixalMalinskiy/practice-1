9c05d29456c9aed8148fb92b11a3f9276b6b5a62
Your Name <you@example.com>
2022-01-25 19:17:11 +0200

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/pages',[\App\Http\Controllers\PageController::class,"index"]);
+Route::post('/pages', [\App\Http\Controllers\PageController::class, "addMessage"]);
+
+
+
